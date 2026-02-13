6893bc37b28d65d63898e8abacae6c4a9a378728
Juan Martinez <mcplux@outlook.com>
2026-02-12 21:26:23 -0500

    Create custom user

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "apps.accounts",
 ]
 
 MIDDLEWARE = [
@@ -115,3 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+
+# Custom User
+AUTH_USER_MODEL = "accounts.User"
