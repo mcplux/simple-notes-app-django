e225ec91a2715a7595b74e486d7786d74a2d25e9
Juan Martinez <mcplux@outlook.com>
2026-02-13 10:09:42 -0500

    Add register view

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.http import HttpRequest
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .forms import RegisterForm
+
+
+def register_view(request: HttpRequest):
+    # Redirect if user is authenticated
+    if request.user.is_authenticated:
+        return redirect("/")
+
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("/")
+
+    else:
+        form = RegisterForm()
+
+    return render(request, "accounts/register.html", {"form": form})
