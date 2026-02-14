774d841ef5d3616d607ad7755a9b03c74ca718d6
Juan Martinez <mcplux@outlook.com>
2026-02-14 17:33:37 -0500

    Add modal

--- apps/notes/views.py
+++ apps/notes/views.py
@@ -7,5 +7,5 @@ from .models import Note
 
 @require_http_methods(["GET"])
 def note_list(request: HttpRequest):
-    notes = Note.objects.filter(user=request.user)
+    notes = Note.objects.filter(user=request.user).order_by("-created_at")
     return render(request, "notes/note_list.html", {"notes": notes})
