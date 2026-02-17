f26698a5a82a0bb6ecbd3b3acc3825a300818afc
Juan Martinez <mcplux@outlook.com>
2026-02-16 20:55:30 -0500

    Minor changes

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -8,14 +8,14 @@ from .forms import RegisterForm
 def register_view(request: HttpRequest):
     # Redirect if user is authenticated
     if request.user.is_authenticated:
-        return redirect("/")
+        return redirect("notes:list")
 
     if request.method == "POST":
         form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect("/")
+            return redirect("notes:list")
 
     else:
         form = RegisterForm()
